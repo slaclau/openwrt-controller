561b5fd0ec2cc92a803b79ef3694461170c34a10
slaclau <77557628+slaclau@users.noreply.github.com>
2023-07-26 23:38:34 +0100

    Update wireless.py

---
+++ src/core/devices/wireless.py
@@ -0,0 +1,19 @@
+
+
+class Wireless:
+    def __init__(self, name, params):
+        self.name = name
+        self.params = params
+        
+    @staticmethod
+    def _enumerate(device):
+        wirelesses = device.uci_request("get_all", ["wireless"])["result"]
+        return wirelesses
+
+    @staticmethod
+    def enumerate(device):
+        _wirelesses = Network._enumerate(device)
+        wirelesses = []
+        for wireless in _wirelesses:
+            wirelesses.append(Wireless(wireless, _wirelesses[wireless]))
+        return wirelesses
