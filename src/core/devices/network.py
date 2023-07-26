f67b5eae41e6670bbda02a0caaff82cb973fa6d0
slaclau <77557628+slaclau@users.noreply.github.com>
2023-07-26 14:53:30 +0100

    Update network.py

--- src/core/devices/network.py
+++ src/core/devices/network.py
@@ -3,5 +3,5 @@ import device
 class Network:
     @staticmethod
     def enumerate(device):
-        networks = device.uci_request("get_all", ["network"])
+        networks = device.uci_request("get_all", ["network"])["result"]
         return networks.keys()
