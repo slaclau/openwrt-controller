f43edde8db211fbf8b331214b269d2534571ba82
slaclau <77557628+slaclau@users.noreply.github.com>
2023-07-26 17:10:19 +0100

    Update test.py

--- src/core/devices/test.py
+++ src/core/devices/test.py
@@ -1,13 +1,8 @@
 import device
-import network as nw
+import network
 dev = device.Device("192.168.1.1")
 dev.login()
 networks = nw.Network.enumerate(dev)
 
-for network in networks:
-    print(network.params)
-
-interfaces = nw.Interface._enumerate(dev)
-print(interfaces)
-for interface in interfaces:
-    print(interface)
+for nw in networks:
+    print(nw.params)
