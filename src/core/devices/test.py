34bdbdcb65bbf6d3c0d57a1106eebe6a7868cea5
slaclau <77557628+slaclau@users.noreply.github.com>
2023-07-26 23:39:21 +0100

    Update test.py

--- src/core/devices/test.py
+++ src/core/devices/test.py
@@ -2,9 +2,15 @@
 
 import device
 import network
+import wireless
 dev = device.Device("192.168.1.1")
 dev.login()
+
 networks = network.Network.enumerate(dev)
+wirelesses = wireless.Wireless.enumerate(dev)
 
 for nw in networks:
     print(nw.params)
+
+for wl in wirelesses:
+    print(wl.params)
