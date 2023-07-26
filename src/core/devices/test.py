e4ca7fdf501e09254e6003a64e59ee93d9fd4cf0
slaclau <77557628+slaclau@users.noreply.github.com>
2023-07-26 23:21:35 +0100

    Update test.py

--- src/core/devices/test.py
+++ src/core/devices/test.py
@@ -4,7 +4,7 @@ import device
 import network
 dev = device.Device("192.168.1.1")
 dev.login()
-networks = nw.Network.enumerate(dev)
+networks = network.Network.enumerate(dev)
 
 for nw in networks:
     print(nw.params)
