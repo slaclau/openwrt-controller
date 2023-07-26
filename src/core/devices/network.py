5dbe0f0f33f23461c8f09323092259cba76245f6
slaclau <77557628+slaclau@users.noreply.github.com>
2023-07-26 17:06:05 +0100

    Update network.py

--- src/core/devices/network.py
+++ src/core/devices/network.py
@@ -25,6 +25,6 @@ class Interface:
     
     @staticmethod
     def _enumerate(device):
-        interfaces = device.sys_request("net.deviceinfo", "")["error"]
+        interfaces = device.sys_request("net.deviceinfo", [])["error"]
         return interfaces
     
