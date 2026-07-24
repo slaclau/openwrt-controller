36e657bd0642c87a08c011d96b511df6555ff8e3
Sebastien Laclau <seb.laclau@gmail.com>
2026-07-24 14:28:51 +0100

    feat(site-manager): add refresh token as cookie and add refresh endpoint

--- site-manager/backend/app/auth.py
+++ site-manager/backend/app/auth.py
@@ -1,12 +1,18 @@
 from datetime import datetime, timedelta, timezone
 import logging
 from typing import TYPE_CHECKING, Annotated
+import uuid
 
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import (
+    APIKeyCookie,
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestForm,
+)
 import jwt
 from pwdlib import PasswordHash
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
 from pydantic import BaseModel
+from sqlmodel import Field, SQLModel
 
 from .dependencies import SessionDep
 from .users.model import User, UserInDb
@@ -14,6 +20,7 @@ from .users.model import User, UserInDb
 logger = logging.getLogger(f"uvicorn.{__name__}")
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 with open("private.pem", "rb") as f:
     private_key = f.read()
@@ -24,6 +31,7 @@ with open("public.pem", "rb") as f:
 
 password_hash = PasswordHash.recommended()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+cookie_scheme = APIKeyCookie(name="refresh_token")
 
 FAKE_HASH = password_hash.hash("password")
 
@@ -31,10 +39,20 @@ FAKE_HASH = password_hash.hash("password")
 class Token(BaseModel):
     access_token: str
     token_type: str
+    expires_in: int = ACCESS_TOKEN_EXPIRE_MINUTES * 60
 
 
-class TokenData(BaseModel):
-    username: str | None = None
+class TokenData(SQLModel, table=False):
+    username: str = Field(foreign_key="userindb.username")
+
+
+class RefreshTokenData(TokenData, table=True):
+    jti: uuid.UUID = Field(primary_key=True, default_factory=uuid.uuid4)
+    expires: datetime = Field(
+        default_factory=lambda: (
+            datetime.now(tz=timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+        )
+    )
 
 
 auth = APIRouter(prefix="/auth")
@@ -67,12 +85,9 @@ def authenticate_user(username, password, session: SessionDep) -> User:
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(
-            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode.update({"exp": expire})
+        expire = datetime.now(tz=timezone.utc) + expires_delta
+        to_encode.update({"exp": expire})
+
     encoded_jwt = jwt.encode(to_encode, private_key, algorithm="EdDSA")
     return encoded_jwt
 
@@ -90,7 +105,6 @@ async def get_current_user(
         username = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
     except jwt.InvalidTokenError:
         raise credentials_exception
     user = session.get(UserInDb, username)
@@ -109,15 +123,81 @@ async def get_current_active_user(
 
 @auth.post("/token")
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep,
+    response: Response,
 ) -> Token:
     user = authenticate_user(form_data.username, form_data.password, session=session)
+    return await create_token_pair(user, session, response)
 
+
+async def create_token_pair(
+    user: User, session: SessionDep, response: Response
+) -> Token:
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    refresh_token_data = RefreshTokenData(username=user.username)
+    session.add(refresh_token_data)
+    session.commit()
+
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={
+            "sub": refresh_token_data.username,
+            "type": "access",
+            "jti": str(refresh_token_data.jti),
+        },
+        expires_delta=access_token_expires,
     )
-    return Token(access_token=access_token, token_type="bearer")
+    refresh_token = create_access_token(
+        data={
+            "sub": refresh_token_data.username,
+            "type": "refresh",
+            "jti": str(refresh_token_data.jti),
+            "exp": refresh_token_data.expires,
+        },
+    )
+    response.set_cookie(
+        "refresh_token",
+        refresh_token,
+        expires=refresh_token_data.expires.astimezone(tz=timezone.utc),
+        secure=True,
+        httponly=True,
+        samesite="strict",
+    )
+    return Token(
+        access_token=access_token,
+        token_type="bearer",
+    )
+
+
+@auth.post("/refresh")
+async def refresh_token(
+    session: SessionDep, response: Response, refresh_token: str = Depends(cookie_scheme)
+) -> Token:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(refresh_token, public_key, algorithms=["EdDSA"])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except jwt.InvalidTokenError:
+        raise credentials_exception
+    user = session.get(UserInDb, username)
+    if not user:
+        raise credentials_exception
+    refresh_token_data = session.get(
+        RefreshTokenData, uuid.UUID(hex=payload.get("jti"))
+    )
+    if not refresh_token_data:
+        raise credentials_exception
+    session.delete(refresh_token_data)
+    session.commit()
+
+    return await create_token_pair(user, session, response)
 
 
 class PermissionChecker:
