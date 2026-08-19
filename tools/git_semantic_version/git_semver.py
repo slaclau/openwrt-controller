b9da15b6eec3a53b7c969cec182cebd9f5c28477
Sebastien Laclau <seb.laclau@gmail.com>
2026-08-19 23:38:00 +0100

    fix(tools): allow git-semver to work in sdist using PKG-INFO

--- tools/git_semantic_version/git_semver.py
+++ tools/git_semantic_version/git_semver.py
@@ -7,7 +7,10 @@ import git
 
 def get_commits(target_dir):
     repo = git.Repo(".", search_parent_directories=True)
+    print(repo)
     commits = repo.iter_commits(paths=["--", os.path.abspath(target_dir)])
+    commits = list(commits)
+    commits.reverse()
     return commits
 
 def _get_version_triplet(commits, initial=(0, 0, 0)):
@@ -38,7 +41,18 @@ def get_version_triplet(target_dir):
     return _get_version_triplet(commits)
 
 def get_version_string(target_dir="."):
-    return ".".join([str(v) for v in get_version_triplet(target_dir)])
+    try:
+        repo = git.Repo(".", search_parent_directories=True)
+        version = ".".join([str(v) for v in get_version_triplet(target_dir)])
+    except (git.exc.InvalidGitRepositoryError, git.exc.NoSuchPathError):
+        if os.path.exists("PKG-INFO"):
+            with open("PKG-INFO") as f:
+                for line in f:
+                    if line.startswith("Version:"):
+                        return line.split(":", 1)[1].strip()
+        else:
+            version = "unknown"
+    return version
 
 def main():
     if sys.argv:
