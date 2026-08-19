79c20768fe4e82b59b77195f71f519078a17b87b
Sebastien Laclau <seb.laclau@gmail.com>
2026-08-19 15:59:14 +0100

    feat(tools): add semver script

---
+++ tools/git_semantic_version/main.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+import re
+import sys
+import os
+
+import git
+
+def get_commits(target_dir):
+    repo = git.Repo(".", search_parent_directories=True)
+    commits = repo.iter_commits(paths=["--", os.path.abspath(target_dir)])
+    return commits
+
+def get_version_triplet(commits, initial=(0, 0, 0)):
+    major, minor, patch = initial
+    for commit in commits:
+        message_type = re.sub(r'\(.*\)', '', commit.summary.split(":")[0]).replace(" ", "")
+        if major > 0:
+            if "!" in message_type or "BREAKING-CHANGE" in commit.trailers_dict:
+                major += 1
+                minor = 0
+                patch = 0
+            elif message_type == "feat":
+                minor += 1
+                patch = 0
+            else:
+                patch += 1
+        else:
+            if "!" in message_type or message_type == "feat" or "BREAKING-CHANGE" in commit.trailers_dict:
+                minor += 1
+                patch = 0
+            else:
+                patch += 1
+
+    return (major, minor, patch)
+
+if __name__ == "__main__":
+    commits = get_commits(os.path.abspath(sys.argv[1]))
+    print(get_version_triplet(commits))
