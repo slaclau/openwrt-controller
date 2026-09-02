0190949b4fd44a7b0d60f1b6ee54df7a3a2bba7c
Sebastien Laclau <seb.laclau@gmail.com>
2026-09-02 19:11:27 +0200

    fix(gen_openapi): make cli work correctly

--- tools/generate_openapi_spec/gen_openapi.py
+++ tools/generate_openapi_spec/gen_openapi.py
@@ -1,5 +1,9 @@
 import json
 from app.main import app
 
-with open("openapi.json", "w") as f:
-    json.dump(app.openapi(), f, indent=2)
+def main():
+    with open("openapi.json", "w") as f:
+        json.dump(app.openapi(), f, indent=2)
+
+if __name__ == "__main__":
+    main()
